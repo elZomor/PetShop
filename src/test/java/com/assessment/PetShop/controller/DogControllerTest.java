0a625d61d606960c0ba2b36be8f10baf53b3f9cc
Zomor <mohamed.elzomor@outlook.com>
2021-09-16 06:14:38 +0200

    - Add unit test for (findAll)

--- src/test/java/com/assessment/PetShop/controller/DogControllerTest.java
+++ src/test/java/com/assessment/PetShop/controller/DogControllerTest.java
@@ -1,37 +1,77 @@
 package com.assessment.PetShop.controller;
 
+import com.assessment.PetShop.domain.Dog;
+import com.assessment.PetShop.repo.DogRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@WebMvcTest(DogController.class)
 class DogControllerTest {
+    @Autowired
+    MockMvc mockMvc ;
+    @Autowired
+    ObjectMapper mapper ;
+
+    @MockBean
+    DogRepository dogRepository;
+
+    Dog dog_1 = new Dog("balady" , 'm');
+    Dog dog_2 = new Dog("wolf" , 'f');
+    Dog dog_3 = new Dog("wolf" , 'm');
 
     @Test
-    void getAllDogs() {
+    void getAllDogs() throws Exception{
+        List<Dog> dogs = new ArrayList<>(Arrays.asList(dog_1,dog_2,dog_3));
+        Mockito.when(dogRepository.findAll()).thenReturn(dogs);
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/dogs")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$",hasSize(3)))
+                .andExpect(jsonPath("$[2].breed", Matchers.is("wolf")));
     }
 
-    @Test
-    void getSingleDog() {
-    }
-
-    @Test
-    void getDogsByBreed() {
-    }
-
-    @Test
-    void createDog() {
-    }
-
-    @Test
-    void updateDogPartially() {
-    }
-
-    @Test
-    void updateDog() {
-    }
-
-    @Test
-    void deleteDog() {
-    }
+//    @Test
+//    void getSingleDog() {
+//    }
+//
+//    @Test
+//    void getDogsByBreed() {
+//    }
+//
+//    @Test
+//    void createDog() {
+//    }
+//
+//    @Test
+//    void updateDogPartially() {
+//    }
+//
+//    @Test
+//    void updateDog() {
+//    }
+//
+//    @Test
+//    void deleteDog() {
+//    }
 }
